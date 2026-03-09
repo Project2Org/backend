ca64facf320e11ff14a024616cc17234d70eab01
Ruben <125226745+ayeeeruben@users.noreply.github.com>
2026-03-09 08:21:12 +0000

    feat: added config file and folder

---
+++ src/main/java/com/project2/calendar/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.project2.calendar.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+      .allowedOrigins(
+        "http://localhost:5173",
+        "https://frontend-cwvc.onrender.com"
+      )
+      .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+      .allowedHeaders("*")
+      .allowCredentials(true);
+  }
+}
